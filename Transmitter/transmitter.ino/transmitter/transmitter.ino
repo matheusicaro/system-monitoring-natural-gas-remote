bfb162017b3c0606e7c89a79fb7d0456eddea70f
matheusicaro <matheusicaro2@hotmail.com>
2018-04-21 18:16:55 -0300

    test new library for real volume.

--- Transmitter/transmitter.ino/transmitter/transmitter.ino
+++ Transmitter/transmitter.ino/transmitter/transmitter.ino
@@ -1,29 +1,67 @@
+#include <LCD5110_Basic.h>
 #include <VirtualWire.h>
-
 
 #undef int
 #undef abs
 #undef double
 #undef float
 #undef round
+
+//-----------------------------------CONFIG--DISPLAY------------------------------------
+  // pin 8 - Serial clock out (SCLK)
+  // pin 9 - Serial data out (DIN)
+  // pin 10 - Data/Command select (D/C)
+  // pin 11 - LCD chip select (CS/CE)
+  // pin 12 - LCD reset (RST)
+  
+  LCD5110 tela(8,9,10,12,11);
+  
+  extern uint8_t SmallFont[];
+  extern uint8_t MediumNumbers[];
+  extern uint8_t BigNumbers[];
+//---------------------------------------FIM-------------------------------------------
+
+
+
 String client = "c-1";
 int volume = 0;
+
+
 
 void setup() {
   Serial.begin(9600);
 
-  attachInterrupt(0,readingVolume,RISING);        //interrupção programada no arduino
+   //---------------------------------------DISPLAY---------------------------------------
+            tela.InitLCD(60);
 
-  // Initialise the IO and ISR
-  vw_set_ptt_inverted(true);      // Required for RF Link module
-  vw_setup(2000);                 // Bits per sec
-  vw_set_tx_pin(7);              // pin 7 ARDUINO-UNO
+            tela.setFont(SmallFont); //Definindo a fonte
+            tela.clrScr(); //Apaga o contéudo que estiver na tela
+      
+            tela.setContrast(60);
+            tela.print("RECEPTOR", CENTER, 0);
+            tela.print("Volume:", LEFT, 20);
+            tela.setFont(MediumNumbers);
+
+   //---------------------------------------FIM-------------------------------------------
+
+    attachInterrupt(0,readingVolume,RISING);        //interrupção programada no arduino
+
+   //---------------------------------------RF-433--------------------------------------
+            vw_set_ptt_inverted(true);      // Required for RF Link module
+            vw_setup(2000);                 // Bits per sec
+            vw_set_tx_pin(7);              // pin 7 ARDUINO-UNO
+   //---------------------------------------FIM-------------------------------------------
+
 }
 
 void loop() {
-  
-    delay(3000);
-    Serial.println(volume/2);   //divisão necessaria para relaçao da media
+    
+    tela.setFont(MediumNumbers);
+    tela.printNumI(0, CENTER, 30);
+    delay(2000);
+
+    //Serial.println(volume/2);   //divisão necessaria para relaçao da media
+    
     sendCliente();
     sendLeitura();
     
@@ -74,5 +112,3 @@ void sendLeitura() {
     delay(200);
 }
 /*----------------------------------------------------------------------------------------*/
-
-
